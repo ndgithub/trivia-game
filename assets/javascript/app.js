a3b1e1ed531c826eb33d695075768584c03257ba
ndgithub <nickydesai.mail@gmail.com>
2019-01-27 21:29:26 -0600

    Changes questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
 var question_one = {
-    question: "function whatDoesItDo(color){\nif (color !== 'blue' || color !== 'green') {\ncolor = 'red';\n}\nreturn color;\n}",
-    answers: ["Something", "Something Else", "3", "4"],
-    correctIndex: 0,
-}
-
-var question_two = {
-    question: "2 What is the answer?",
-    answers: ["1", "2", "3", "4"],
-    correctIndex: 1,
-}
-
-var question_three = {
-    question: "var myVar = 4;console.log(76);",
-    answers: ["1", "2", "3", "4"],
+    question: "Where does Jim propose to Pam?",
+    answers: ["A Restaurant", "The Office", "A Gas Station", "Niagara Falls"],
     correctIndex: 2,
 }
 
+var question_two = {
+    question: "What does Michael Scott put in his scotch?",
+    answers: ["Water", "Salt", "Splenda", "Vanilla"],
+    correctIndex: 2,
+}
+
+var question_three = {
+    question: "Who hosts a barbecue that is Michael NOT invited to?",
+    answers: ["Jim", "Pam", "Ryan", "Dwight"],
+    correctIndex: 0,
+}
+
 var question_four = {
-    question: "4 What is the answer?",
-    answers: ["1", "2", "3", "4"],
+    question: "What character does Dwight dress up as on Earth Day?",
+    answers: ["The Green Machine", "The Solar Son", "Energy Man", "Recyclops"],
     correctIndex: 3,
 }
 
@@ -33,12 +33,7 @@ var ui = {
     timer: function (secondsRemaining) {
         $("#timer").html(secondsRemaining + "");
     },
-    question: function (questionText) {
-        $("#question").html(questionText);
-    },
-    answers: function (questionText) {
-        $("#answers-container").html(questionText);
-    },
+    
 }
 
 var game = {
@@ -50,14 +45,14 @@ var game = {
     init: function () {
         $(".your-class").slick({
             // Slick Settings
-            swipe:false,
+            swipe: false,
             arrows: false,
-            draggable:false,
+            draggable: false,
         });
         $("#start-button").on("click", function () {
             game.goToNextQuestion();
         });
-        
+
     },
     goToNextQuestion: function () {
         this.currentQuestionIndex++;
@@ -66,35 +61,26 @@ var game = {
         var currQuestion = this.questions[this.currentQuestionIndex];
 
         ui.timer(this.time);
-        ui.question(currQuestion.question);
-        ui.answers("");
         ui.dialogue("");
 
         //Display answer
-        var slideHtml = $("<div>").addClass("aSlide");
-        var styledCode = $("<code>").addClass("question").text(currQuestion.question);
-        var questionContainer = $("<pre>").append(styledCode);
-        var answersContainer = $("<div>").addClass("answers-container");
 
+
+        
+
+        var questionContainer = $("<h3>").text(currQuestion.question);
+
+        var answersContainer = $("<div>").addClass("answers-container");
         for (var i = 0; i < currQuestion.answers.length; i++) {
             var answerChoice = $("<div>").addClass("answer-choice").attr("answerNumber", i).html(currQuestion.answers[i]);
             answersContainer.append(answerChoice);
         }
 
+        var slideHtml = $("<div>").addClass("aSlide");
         slideHtml.append(questionContainer, answersContainer);
-        $(".your-class").slick("slickAdd",slideHtml);
+        $(".your-class").slick("slickAdd", slideHtml);
         $(".your-class").slick("slickNext");
-        
-        $('pre code').each(function(i, block) {
-            hljs.highlightBlock(block);
-          });
-          hljs.configure({useBR: true});
-          $('div.code').each(function(i, block) {
-            hljs.highlightBlock(block);
-          });
-          
 
-        
         var questionTimer = setInterval(function () {
             game.time--;
             ui.timer(game.time);
@@ -115,7 +101,7 @@ var game = {
             } else {
                 var currentQuestion = game.questions[game.currentQuestionIndex];
                 var correctAnswer = currentQuestion.answers[currentQuestion.correctIndex];
-                $("#dialogue-box").append("Incorrect. The correct answer was:");
+                $("#dialogue-box").append("Incorrect. The correct answer was: ");
                 $("#dialogue-box").append(correctAnswer);
                 game.questionOver();
             }
@@ -126,7 +112,7 @@ var game = {
     gameOver: function () {
         ui.dialogue("Game Over <br>You got "
             + game.totalCorrect + " out of " +
-            game.questions.length + 'correct<br> <div id ="reset-button">Play Again?</button>');
+            game.questions.length + ' correct<br> <div id ="reset-button">Play Again?</button>');
         $("#reset-button").on("click", game.reset);
     },
     questionOver: function () {
@@ -149,3 +135,13 @@ var game = {
 }
 
 game.init();
+
+
+
+
+
+
+
+
+
+
